059401b72e7e6d728577ac331ad487b325745cc7
TEAM ISTKHAR <mdistkhar1078@gmail.com>
2025-05-19 00:01:59 +0530

    Update ping.py

--- AnieXEricaMusic/plugins/tools/ping.py
+++ AnieXEricaMusic/plugins/tools/ping.py
@@ -1,28 +1,36 @@
 from datetime import datetime
-
 from pyrogram import filters
-from pyrogram.types import Message
-
+from pyrogram.types import Message, InlineKeyboardMarkup
 from AnieXEricaMusic import app
-from AnieXEricaMusic.core.call import AMBOT
+from AnieXEricaMusic.core.call import RAUSHAN
 from AnieXEricaMusic.utils import bot_sys_stats
+from AnieXEricaMusic.utils.inline.extras import botplaylist_markup
 from AnieXEricaMusic.utils.decorators.language import language
-from AnieXEricaMusic.utils.inline import supp_markup
 from config import BANNED_USERS, PING_IMG_URL
+import aiohttp
+import asyncio
 
 
-@app.on_message(filters.command(["ping", "alive"]) & ~BANNED_USERS)
+@app.on_message(filters.command(["ping", "alive"]) & ~BANNED_USERS & filters.group)
 @language
 async def ping_com(client, message: Message, _):
-    start = datetime.now()
     response = await message.reply_photo(
         photo=PING_IMG_URL,
         caption=_["ping_1"].format(app.mention),
     )
+    start = datetime.now()
     pytgping = await AMBOT.ping()
     UP, CPU, RAM, DISK = await bot_sys_stats()
     resp = (datetime.now() - start).microseconds / 1000
-    await response.edit_text(
-        _["ping_2"].format(resp, app.mention, UP, RAM, CPU, DISK, pytgping),
-        reply_markup=supp_markup(_),
+    await response.edit(
+        _["ping_2"].format(
+            resp,
+            app.mention,
+            UP,
+            RAM,
+            CPU,
+            DISK,
+            pytgping,
+        ),
+        reply_markup=InlineKeyboardMarkup(botplaylist_markup(_)),
     )
